6984184a1e134851df52d5595cc566e7725e65d8
kerrigan-lin <kerrigan_lin@trendmicro.com>
2019-12-09 12:03:10 +0800

    added list

--- src/lambda/lambda_function.py
+++ src/lambda/lambda_function.py
@@ -40,9 +40,9 @@ def lambda_handler(event, context):
     if 'list' == RequestBody['queryResult']['intent']['displayName']:
         instances = ec2.instances.filter(
             Filters=[{'Name': 'instance-state-name', 'Values': ['running', 'stopped']}, {'Name': 'tag:Environment', 'Values': ['Staging']}])
-        returnMessage = ''
+        returnMessage = '以下是已經被北極熊這隻熊控管的機器清單:'
         for instance in instances:
-            returnMessage = returnMessage + '以下是已經被北極熊這隻熊控管的機器清單:' + \
+            returnMessage = returnMessage + \
                 str(instance.id) + ' (' + str(instance.state) + ')\n'
         return {
             'statusCode': 200,
